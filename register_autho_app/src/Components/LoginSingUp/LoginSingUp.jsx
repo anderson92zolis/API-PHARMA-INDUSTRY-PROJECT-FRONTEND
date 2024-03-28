b83f93d58a88951d81e5ffa97a8d53223756ae60
anderson92zolis <azolis92@hotmail.es>
2024-03-28 17:46:14 +0100

    adding the react Login and sig up

--- register_autho_app/src/Components/LoginSingUp/LoginSingUp.jsx
+++ register_autho_app/src/Components/LoginSingUp/LoginSingUp.jsx
@@ -1,20 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import "./LoginSingUp.css";
 import user_icon from "../Assets/person.png";
 import password_icon from "../Assets/password.png";
 import email_icon from "../Assets/email.png";
+
 const LoginSingUp = () => {
+
+  const [action, setAction] = useState("Sing Up")
+
   return (
     <div className="container">
       <div className="header">
-        <div className="text"> Sing Up</div>
+        <div className="text"> {action}</div>
         <div className="underline"> </div>
       </div>
       <div className="inputs">
-        <div className="input">
+       {action==="Login"?<div></div>: <div className="input">
           <img src={user_icon} alt="" />
           <input type="text" placeholder="Name"/>
-        </div>
+        </div>}        
         <div className="input">
           <img src={email_icon} alt="" />
           <input type="email" placeholder="Email"/>
@@ -24,10 +28,12 @@ const LoginSingUp = () => {
           <input type="password" placeholder="Password" />
         </div>
       </div>
-      <div className="forgot-password"> Lost password <span>clic here</span></div>
+      {action==="Sign Up"?<div></div>: <div className="forgot-password"> Lost password <span>clic here!</span></div>}     
+
+      
       <div className="submit-container">
-        <div  className="submit">Submit up</div>
-        <div  className="submit">Login</div>
+        <div  className={action==="Login"?"submit gray": "submit"} onClick={()=> {setAction("Sign Up")}}>Submit up</div>
+        <div  className={action==="Sign Up"? "submit gray":"submit"}  onClick={()=> {setAction("Login")}} >Login</div>
 
       </div>  
 
