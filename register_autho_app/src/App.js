02cfebb9c92528526e84ec555ceb96cba6b0c17c
anderson92zolis <azolis92@hotmail.es>
2024-05-26 01:16:08 +0200

    adding the maco to calculate

--- register_autho_app/src/App.js
+++ register_autho_app/src/App.js
@@ -3,7 +3,9 @@ import SignupPage from "./Components/LoginSingUp/SingUp";
 import LoginPage from "./Components/LoginSingUp/Login";
 import Dashboard from "./Components/LoginSingUp/WelcomeDashboard";
 
+
 import OptionOneForm from './Components/LoginSingUp/OptionOneForm';
+import DosisDiariaTerapeutica from './Components/LoginSingUp/dosisDiariaTerapeutica';
 
 // https://www.geeksforgeeks.org/spring-security-login-page-with-react/
 
@@ -16,6 +18,7 @@ function App() {
           <Route path="/signup" element={<SignupPage />} />
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/dashboard/option-one" element={<OptionOneForm />} />
+          <Route path="/dashboard/maco" element={<DosisDiariaTerapeutica />} />
           
         </Routes>
       </Router>
